4edbfec2939beef629abad5237763f2accabd773
tlegares <tlegares@btslivh.eu>
2021-01-11 14:26:46 +0100

    Premiere ligne

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: legares
+ *
+ * Created on 11 janvier 2021, 14:22
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    cout << "Bonjour" << endl;
+    
+
+    return 0;
+}
+
